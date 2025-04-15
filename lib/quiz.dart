b862965678c3215c12f3647f08796d797cb478e4
RobbiHobbiti <Robbihobbiti@gmail.com>
2025-04-15 20:20:07 +0000

    first commit

---
+++ lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/questions.dart';
+import 'package:quiz/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget{
+ const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+   return _QuizState();
+ }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.deepPurple,
+                  Colors.purple,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )
+          ),
+          child:  activeScreen,
+        ),
+      ),
+    );
+  }
+
+}
